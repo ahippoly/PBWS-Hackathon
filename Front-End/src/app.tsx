72f457e195547f5bfe793de5f54e0da0a4a4a525
Arnaud <arnaud.hippo@gmail.com>
2024-04-06 09:57:25 +0200

    integration with solana done

--- Front-End/src/app.tsx
+++ Front-End/src/app.tsx
@@ -14,19 +14,23 @@ import { Divider, Stack } from '@mui/material'
 import HomePage from './pages/HomePage'
 import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { SolanaWalletProvider } from './components/Web3Related/SolanaWalletProvider'
 
 gsap.registerPlugin(useGSAP)
 
 export function App () {
   return (
-    <MuiThemeProvider>
-      <BrowserRouter>
-        <Stack bgcolor='background.default' direction='column' sx={{ height: '100%', maxHeight: '100%' }}>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-          </Routes>
-        </Stack>
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <SolanaWalletProvider>
+      <MuiThemeProvider>
+        <BrowserRouter>
+          <Stack bgcolor='background.default' direction='column' sx={{ height: '100%', maxHeight: '100%' }}>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+            </Routes>
+          </Stack>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </SolanaWalletProvider>
+
   )
 }
--- Front-End/src/pages/HomePage.tsx
+++ Front-End/src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Stack, Typography } from '@mui/material'
 import { NavLink } from 'react-router-dom'
 import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { SendSOLToRandomAddress } from '@/components/Web3Related/SendToRandomAddress'
 
 function HomePage () {
   return (
@@ -12,7 +13,9 @@ function HomePage () {
         variant='h1'
         id='big-title'
         sx={{ fontSize: '3rem', fontWeight: 'bold', color: 'primary.main' }}
-      />
+      >Title of the product
+      </Typography>
+      <SendSOLToRandomAddress />
     </Box>
   )
 }
